88261a341c4cb36cb5278a58b7520f2d5aedb8e5
Alexis THOMAS <fr.alexisthomas@gmail.com>
2023-08-25 00:34:42 +0200

    feat(android): newer scanFile using MediaStore

--- src/android/GalleryRefresh.java
+++ src/android/GalleryRefresh.java
@@ -1,17 +1,18 @@
 package com.ung.galleryrefresh;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
-import java.io.File;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.content.Context;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.webkit.MimeTypeMap;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.File;
 
 public class GalleryRefresh extends CordovaPlugin {
 
@@ -41,9 +42,40 @@ public class GalleryRefresh extends CordovaPlugin {
 
     private void _scanFile(File contentFile) {
         Uri contentUri = Uri.fromFile(contentFile);
+
+        // deprecated
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         mediaScanIntent.setData(contentUri);
         cordova.getActivity().sendBroadcast(mediaScanIntent);
+
+        // new using MediaStore
+        String mimeType;
+        if (ContentResolver.SCHEME_CONTENT.equals(contentUri.getScheme())) {
+            mimeType = cordova.getActivity().getContentResolver().getType(contentUri);
+        } else {
+            String fileExtension = MimeTypeMap.getFileExtensionFromUrl(contentUri.toString());
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(fileExtension.toLowerCase());
+        }
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DATA, contentFile.getAbsolutePath());
+        Uri externalContentUri = null;
+        if (mimeType != null) {
+            values.put(MediaStore.MediaColumns.MIME_TYPE, mimeType);
+            if (mimeType.contains("audio/")) {
+                externalContentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+            } else if (mimeType.contains("image/")) {
+                externalContentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            } else if (mimeType.contains("video/")) {
+                externalContentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+            } else {
+                if (Build.VERSION.SDK_INT >= 29) {
+                    externalContentUri = MediaStore.Downloads.EXTERNAL_CONTENT_URI;
+                }
+            }
+        }
+        if (externalContentUri != null) {
+            cordova.getActivity().getContentResolver().insert(externalContentUri, values);
+        }
     }
 
     private String _checkFilePath(String filePath) {
